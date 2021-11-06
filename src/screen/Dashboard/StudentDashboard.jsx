4c0fd22e848e46229a3da8035d262d493a187b8a
wishwashan <ravindu.18@cse.mrt.ac.lk>
2021-11-06 12:18:57 +0530

    iconstudentdashboard

--- src/screen/Dashboard/StudentDashboard.jsx
+++ src/screen/Dashboard/StudentDashboard.jsx
@@ -3,25 +3,11 @@ import NavBar from '../../containers/Navbar/Navbar';
 
 import { Link } from "react-router-dom";
 
-class AdminDashboard extends Component {
+class StudentDashboard extends Component {
     constructor(props) {
         super(props);
         this.state = {}
-        this.checkLogin();
     }
-    componentDidMount() {
-        this.checkLogin();
-    }
-    checkLogin() {
-        var type = localStorage.getItem('token');
-        var user = localStorage.getItem('user');
-        if (!type || user != "OfficeClerk") {
-            this.props.history.push("/");
-            return;
-        }
-    }
-
-
     render() {
         return (
             <div>
@@ -36,25 +22,25 @@ class AdminDashboard extends Component {
 
                                 <Link to={"/student/checkAvailability"} >
                                     <a href="" className="btn btn-sq-lg btn-success p-4 m-4">
-                                        <i className="fa fa-plus-square fa-5x"></i><br />
+                                        <i className="fa fa-calendar-check-o fa-5x"></i><br />
                                        Check Availabilty
                                     </a>
                                 </Link>
                                 <Link to={"/student/createNormalRequest"} >
                                     <a href="" className="btn btn-sq-lg btn-success p-4 m-4">
-                                        <i className="fa fa-check-square-o fa-5x"></i><br />
+                                        <i className="fa fa-pencil-square fa-5x"></i><br />
                                         Create Normal Request
                                     </a>
                                 </Link>
                                 <Link to={"/student/createTemporaryRequest"} >
                                     <a href="" className="btn btn-sq-lg btn-success p-4 m-4">
-                                        <i className="fa fa-history fa-5x"></i><br />
+                                        <i className="fa fa-pencil-square fa-5x"></i><br />
                                         Tempory Request
                                     </a>
                                 </Link>
                                 <Link to={"/student/borrowingItems"} >
                                     <a href="" className="btn btn-sq-lg btn-success p-4 m-4">
-                                        <i className="fa fa-search fa-5x"></i><br />
+                                        <i className="fa fa-list fa-5x"></i><br />
                                         Borrowing Item
                                     </a>
                                 </Link>
@@ -74,4 +60,4 @@ class AdminDashboard extends Component {
     }
 }
 
-export default AdminDashboard;
+export default StudentDashboard;
